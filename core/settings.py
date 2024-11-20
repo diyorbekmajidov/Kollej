74a2be49b6c10d53e692ab57c7e893e2e04297ab
Diyorbek <majidovdiyorbek741@gmail.com>
2024-11-20 23:24:48 +0500

    static root

--- core/settings.py
+++ core/settings.py
@@ -118,11 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'  # Static fayllar uchun URL prefiksi
+MEDIA_URL = '/media/'    # Media fayllar uchun URL prefiksi
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Static fayllar uchun sozlamalar
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Development muhitida ishlatiladi
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   # Production muhitida collectstatic yig‘adi
+
+# Media fayllar uchun sozlamalar
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Media fayllarning saqlanish joyi
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
